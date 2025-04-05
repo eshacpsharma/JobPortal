e22831f6cbb82f902b646f95a7db78b622205d40
Esha Sharma <eshacpsharma@gmail.com>
2025-04-05 11:34:52 +0530

    Initial Commit

---
+++ Job_Search_Platform/src/test/java/com/example/job_search_platform/JobSearchPlatformApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.job_search_platform;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchPlatformApplicationTests {
+
+    // @Test
+    // void contextLoads() {
+    // }
+
+}
